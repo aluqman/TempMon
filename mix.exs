db786633b44ca976ddf8bb034ebf6ab1e09c89b1
Raven <aluqman@hawk.iit.edu>
2023-03-30 20:13:43 -0500

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Gpio.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gpio,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Gpio.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+			{:ecto, "~> 3.0"},
+			{:postgrex, "<= 0.15.11"},
+			{:dht, "~> 0.1"}
+    ]
+  end
+end
