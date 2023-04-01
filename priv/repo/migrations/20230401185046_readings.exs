24ad57697135830b7941df39cfeb3e86073b90d2
Raven <aluqman@hawk.iit.edu>
2023-04-01 13:54:36 -0500

    Added migration for sensor readings

--- priv/repo/migrations/20230401185046_readings.exs
+++ priv/repo/migrations/20230401185046_readings.exs
@@ -2,6 +2,10 @@ defmodule TempMon.Repo.Migrations.Readings do
   use Ecto.Migration
 
   def change do
-
+		create table(:readings) do
+			add :temperature, :float
+			add :humidity, :float
+			timestamps()
+		end
   end
 end
