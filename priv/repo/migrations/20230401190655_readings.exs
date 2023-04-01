694e1406624886be675ef3a8258c7c5ee6eeb3cc
Raven <aluqman@hawk.iit.edu>
2023-04-01 14:13:40 -0500

    Added readings table

--- priv/repo/migrations/20230401190655_readings.exs
+++ priv/repo/migrations/20230401190655_readings.exs
@@ -5,7 +5,7 @@ defmodule TempMon.Repo.Migrations.Readings do
 		create table(:readings, primary_key: false) do
 			add :temperature, :float
 			add :humidity, :float
-			timestamps()
+			timestamps(updated_at: false, type: :utc_datetime)
 		end
   end
 end
