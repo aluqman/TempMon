50a2a27b248d51f8552ee08699142391214a1c20
Raven <aluqman@hawk.iit.edu>
2023-04-02 14:13:32 -0500

    Added sensor behaviour

---
+++ lib/gpio/sensor.ex
@@ -0,0 +1,18 @@
+defmodule TempMon.Sensor do
+  @moduledoc """
+  Abstraction over a sensor.
+
+  This is used to implement a behavior for a sensor, delegating to
+  whatever is configurd by the application.
+  """
+
+  @callback read(integer(), atom()) :: {:ok, map()} | {:error, integer()}
+
+  @doc """
+  Read sensor data from a pin. Returns either {:ok, map} where
+  map contains sensor data, or {:error, error_code}.
+  """
+  def read(pin, sensor), do: impl().read(pin, sensor)
+
+  defp impl, do: Application.get_env(:tempmon, :sensor_lib, DHT)
+end
