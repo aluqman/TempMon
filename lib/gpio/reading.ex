e09c6736934a06eb6e26e1cb718619eceb92aad9
Raven <aluqman@hawk.iit.edu>
2023-04-02 14:14:16 -0500

    Added processing function for reading

--- lib/gpio/reading.ex
+++ lib/gpio/reading.ex
@@ -1,17 +1,40 @@
 defmodule TempMon.Reading do
+  @moduledoc """
+  Schema and changeset for the reading of data from a sensor. 
+  Contains information relating solely to temperature and humidity.
+  """
+
   use Ecto.Schema
+	require Logger
 
   @primary_key false
 
   schema "readings" do
     field(:temperature, :float)
     field(:humidity, :float)
-    field(:inserted_at, :utc_datetime)
+    timestamps(updated_at: false, type: :utc_datetime)
   end
 
   def changeset(reading, params \\ %{}) do
     reading
-    |> Ecto.Changeset.cast(params, [:temperature, :humidity, :inserted_at])
-    |> Ecto.Changeset.validate_required([:temperature, :humidity, :inserted_at])
+    |> Ecto.Changeset.cast(params, [:temperature, :humidity])
+    |> Ecto.Changeset.validate_required([:temperature, :humidity])
   end
+
+	@doc """
+	Responsible for processing a reading from a sensor, and inserting
+	properly formatted data in to the database.
+	"""
+	@spec process({:ok, map()} | {:error, integer()}) :: any()
+	def process({:ok, reading}) do
+		Logger.info("Analyzing reading.")
+
+		%TempMon.Reading{}
+		|> changeset(reading)
+		|> TempMon.Repo.insert()
+	end
+
+	def process({:error, error_code}) do
+		Logger.error("Sensor responded with error code #{error_code}.")
+	end
 end
