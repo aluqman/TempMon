0ec9401014b5bdefc1fc4400ef6d9405d122e8e3
Raven <aluqman@hawk.iit.edu>
2023-04-07 16:12:59 -0500

    Satisfy credo --strict

--- test/support/repo_case.ex
+++ test/support/repo_case.ex
@@ -1,5 +1,11 @@
 defmodule TempMon.RepoCase do
+  @moduledoc """
+  Module for assisting with leveraging Ecto when running tests for this application.
+
+  Creates a sandbox database for testing data to avoid contaminating the main database.
+  """
   use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL.Sandbox
 
   using do
     quote do
@@ -12,10 +18,10 @@ defmodule TempMon.RepoCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(TempMon.Repo)
+    :ok = Sandbox.checkout(TempMon.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(TempMon.Repo, {:shared, self()})
+      Sandbox.mode(TempMon.Repo, {:shared, self()})
     end
 
     :ok
